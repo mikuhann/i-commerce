47a9c389b6125ddfa1464d02772b900439dddc0c
mikuhann <ng.kano@gmail.com>
2019-11-12 20:35:58 +0300

    add scuffold homepage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import HomePage from './homepage.component';
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      app works!
+      <HomePage />
     </div>
   );
 }
---
+++ src/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='menu-content'>
+          <h1 className='menu-item-title'>Hats</h1>
+          <span className='menu-item-subtitle'>Shop Now</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='menu-content'>
+          <h1 className='menu-item-title'>Jackets</h1>
+          <span className='menu-item-subtitle'>Shop Now</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='menu-content'>
+          <h1 className='menu-item-title'>Sneakers</h1>
+          <span className='menu-item-subtitle'>Shop Now</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='menu-content'>
+          <h1 className='menu-item-title'>Woman's</h1>
+          <span className='menu-item-subtitle'>Shop Now</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='menu-content'>
+          <h1 className='menu-item-title'>Mans</h1>
+          <span className='menu-item-subtitle'>Shop Now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
