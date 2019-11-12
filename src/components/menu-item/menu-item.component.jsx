fcb2bc1c2cfbc7e0641ee66bef2c89239a489f1e
mikuhann <ng.kano@gmail.com>
2019-11-12 22:59:26 +0300

    add directory component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const MenuItemComponent = ({title}) => (
+import './menu-item.styles.scss';
+
+const MenuItemComponent = ({ title }) => (
   <div className='menu-item'>
     <div className='menu-item-content'>
-      <h1 className='menu-item-title'>{title}</h1>
+      <h1 className='menu-item-title'>{ title }</h1>
       <span className='menu-item-subtitle'>Shop Now</span>
     </div>
   </div>
