404769f470cfe2602bfd984fff41c954f32a34ec
mikuhann <ng.kano@gmail.com>
2019-11-12 21:01:44 +0300

    fix classes add styles file

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,34 +1,36 @@
 import React from 'react';
 
+import './homepage.styles.scss'
+
 const HomePage = () => (
   <div className='homepage'>
     <div className='directory-menu'>
       <div className='menu-item'>
-        <div className='menu-content'>
+        <div className='menu-item-content'>
           <h1 className='menu-item-title'>Hats</h1>
           <span className='menu-item-subtitle'>Shop Now</span>
         </div>
       </div>
       <div className='menu-item'>
-        <div className='menu-content'>
+        <div className='menu-item-content'>
           <h1 className='menu-item-title'>Jackets</h1>
           <span className='menu-item-subtitle'>Shop Now</span>
         </div>
       </div>
       <div className='menu-item'>
-        <div className='menu-content'>
+        <div className='menu-item-content'>
           <h1 className='menu-item-title'>Sneakers</h1>
           <span className='menu-item-subtitle'>Shop Now</span>
         </div>
       </div>
       <div className='menu-item'>
-        <div className='menu-content'>
+        <div className='menu-item-content'>
           <h1 className='menu-item-title'>Woman's</h1>
           <span className='menu-item-subtitle'>Shop Now</span>
         </div>
       </div>
       <div className='menu-item'>
-        <div className='menu-content'>
+        <div className='menu-item-content'>
           <h1 className='menu-item-title'>Mans</h1>
           <span className='menu-item-subtitle'>Shop Now</span>
         </div>
